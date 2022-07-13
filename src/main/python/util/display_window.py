6f618c2b4d3dbe019ecfd6882a47608119ae4043
Mathew Chan <mchan6466@gmail.com>
2022-07-13 19:42:47 +0800

    use system scaling #4

--- src/main/python/util/display_window.py
+++ src/main/python/util/display_window.py
@@ -1,18 +1,24 @@
 import sys
+# from PyQt5.QtWidgets import QApplication
+# from PyQt5 import QtCore
 if sys.platform == 'win32':
     from ctypes import windll
-
-REFERENCE_WIDTH = 1920
-REFERENCE_HEGHT = 1080
 
 class DisplayWindow():
     def __init__(self, width, height):
         if sys.platform == 'win32':
+            screen_w = windll.user32.GetSystemMetrics(0)
+            screen_h = windll.user32.GetSystemMetrics(1)
+            original_width = screen_w
+            original_height = screen_h
             windll.user32.SetProcessDPIAware()
             screen_w = windll.user32.GetSystemMetrics(0)
             screen_h = windll.user32.GetSystemMetrics(1)
-            self.width = width * screen_w // REFERENCE_WIDTH
-            self.height = height * screen_h // REFERENCE_HEGHT
+            self.width_scale = screen_w//original_width
+            self.height_scale = screen_h//original_height
+            # QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+            self.width = width*self.width_scale
+            self.height = height*self.height_scale
         else:
             self.width = width
             self.height = height
